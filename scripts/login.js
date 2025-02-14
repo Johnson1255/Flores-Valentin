17175785a5bad653d947b66c277e8f81f223df63
Johnson1255 <johnsonc12yt@gmail.com>
2025-02-13 22:37:59 -0500

    add: Añadir pestaña de resgistro para el apartado de login
    Interfaz de registro sin base de datos

---
+++ scripts/login.js
@@ -0,0 +1,19 @@
+const tabButtons = document.querySelectorAll('.tab-button');
+const loginForm = document.querySelector('.login-form');
+const registerForm = document.querySelector('.register-form');
+
+tabButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        tabButtons.forEach(btn => btn.classList.remove('active'));
+        
+        button.classList.add('active');
+        
+        if (button.textContent.trim() === 'Iniciar Sesión') {
+            loginForm.style.display = 'block';
+            registerForm.style.display = 'none';
+        } else {
+            loginForm.style.display = 'none';
+            registerForm.style.display = 'block';
+        }
+    });
+});
